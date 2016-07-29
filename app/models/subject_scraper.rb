dfa1079c215109dad206efe7aab5470660d14604
Kyle Qian <cqian@stanford.edu>
2016-07-29 03:52:20 -0700

    subject_scraper mysteriously ran

--- app/models/subject_scraper.rb
+++ app/models/subject_scraper.rb
@@ -4,26 +4,33 @@ require 'open-uri'
 require 'json'
 require 'time'
 
-listing = Nokogiri::HTML(open("https://explorecourses.stanford.edu/browse")).at_css('.departmentsContainer').children
+def main
+  listing = Nokogiri::HTML(open("https://explorecourses.stanford.edu/browse")).at_css('.departmentsContainer').children
 
-parsed_listing = []
-listing.each do |l|
-  if l.name == 'h2'
-    parsed_listing << {school: l.text.strip, departments: []}
-  elsif l.name == 'ul'
-    l.css('li').each do |d|
-      d_match = /([^\(\)]+) \(([^\(\)]+)\)/.match(d.text.strip)
-      if d_match
-        d_object = {name: d_match[1], code: d_match[2], default: true}
-        parsed_listing[-1][:departments] << d_object
+  parsed_listing = []
+  listing.each do |l|
+    if l.name == 'h2'
+      parsed_listing << {school: l.text.strip, departments: []}
+    elsif l.name == 'ul'
+      l.css('li').each do |d|
+        d_match = /([^\(\)]+) \(([^\(\)]+)\)/.match(d.text.strip)
+        if d_match
+          d_object = {name: d_match[1], code: d_match[2], default: true}
+          parsed_listing[-1][:departments] << d_object
+        end
       end
     end
   end
+
+  # TODO: I'm currently manually deleting some of the last elements
+  # TODO: Also manually turning default courses on and off
+
+  filename = "departments~#{Time.now.iso8601.split('.')[0].gsub(':', '-')}"
+  File.open("#{filename}.json", "w") do |f|
+    f.write(JSON.pretty_generate(parsed_listing))
+  end
 end
 
-# TODO: I'm currently manually deleting some of the last elements
-
-filename = "departments~#{Time.now.iso8601.split('.')[0].gsub(':', '-')}"
-File.open("#{filename}.json", "w") do |f|
-  f.write(JSON.pretty_generate(parsed_listing))
+if __FILE__ == $0
+  main
 end
