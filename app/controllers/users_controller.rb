1f4150cf6a95cab5d46b9b98b03d1e5cae4b91ff
Kyle Qian <cqian@stanford.edu>
2016-07-29 03:45:39 -0700

    Fixed checking if user is new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,15 @@ class UsersController < ApplicationController
   end
 
   def login
+    # param passed from frontend
     @user = User.find_by(email: params[:email])
     default_departments = (@user && @user.subject_settings != nil) ? JSON.parse(@user.subject_settings) : UrlHelper.get_default_departments
     render json: default_departments.to_json
   end
 
   def submit
-    @user = User.find_by(email: params[:email])
+    # param passed from form
+    @user = User.find_by(email: user_params[:email])
     status = nil
     if @user
       status = 'update'
@@ -22,7 +24,7 @@ class UsersController < ApplicationController
 
     if @user.valid?
       @user.save!
-      redirect_to '/users/confirmation', s: status
+      redirect_to controller: :users, action: :confirmation, s: status
       # if @question.email? 
       #   QuestionMailer.send_question(@question).deliver_now
       #   AdminMailer.send_question(@question).deliver_now
