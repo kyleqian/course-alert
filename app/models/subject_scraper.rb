81809676ca432a683a3de19992c948f3dae93953
Kyle Qian <cqian@stanford.edu>
2016-07-29 16:02:16 -0700

    Clean up scaper

--- app/models/subject_scraper.rb
+++ app/models/subject_scraper.rb
@@ -1,38 +1,36 @@
 require 'net/http'
-require 'Nokogiri'
 require 'open-uri'
 require 'json'
 require 'time'
 
-def main
-  listing = Nokogiri::HTML(open("https://explorecourses.stanford.edu/browse")).at_css('.departmentsContainer').children
+class SubjectScraper
+  def self.main
 
-  parsed_listing = []
-  listing.each do |l|
-    if l.name == 'h2'
-      parsed_listing << {school: l.text.strip, departments: []}
-    elsif l.name == 'ul'
-      l.css('li').each do |d|
-        d_match = /([^\(\)]+) \(([^\(\)]+)\)/.match(d.text.strip)
-        if d_match
-          getting_col = l.attribute('title').text.split(' - ')
-          col = getting_col.length > 1 ? getting_col[1].split[1].to_i : 1
-          d_object = {name: d_match[1], code: d_match[2], col: col, default: true}
-          parsed_listing[-1][:departments] << d_object
+    listing = Nokogiri::HTML(open("https://explorecourses.stanford.edu/browse")).at_css('.departmentsContainer').children
+
+    parsed_listing = []
+    listing.each do |l|
+      if l.name == 'h2'
+        parsed_listing << {school: l.text.strip, departments: []}
+      elsif l.name == 'ul'
+        l.css('li').each do |d|
+          d_match = /([^\(\)]+) \(([^\(\)]+)\)/.match(d.text.strip)
+          if d_match
+            getting_col = l.attribute('title').text.split(' - ')
+            col = getting_col.length > 1 ? getting_col[1].split[1].to_i : 1
+            d_object = {name: d_match[1], code: d_match[2], col: col, default: true}
+            parsed_listing[-1][:departments] << d_object
+          end
         end
       end
     end
+
+    # TODO: I'm currently manually deleting some of the last elements
+    # TODO: Also manually turning default courses on and off
+
+    filename = "departments~#{Time.now.iso8601.split('.')[0].gsub(':', '-')}"
+    File.open("#{filename}.json", "w") do |f|
+      f.write(JSON.pretty_generate(parsed_listing))
+    end
   end
-
-  # TODO: I'm currently manually deleting some of the last elements
-  # TODO: Also manually turning default courses on and off
-
-  filename = "departments~#{Time.now.iso8601.split('.')[0].gsub(':', '-')}"
-  File.open("#{filename}.json", "w") do |f|
-    f.write(JSON.pretty_generate(parsed_listing))
-  end
-end
-
-if __FILE__ == $0
-  main
 end
