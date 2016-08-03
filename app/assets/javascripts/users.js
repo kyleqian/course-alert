82a0d21f6526123b4a429725330466aacf77df5e
Kyle Qian <cqian@stanford.edu>
2016-08-03 02:16:18 -0700

    Updated homepage to two column

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,7 +8,15 @@ $(document).on('turbolinks:load', function() {
   var $submitButton = $('#submit-button');  
   var $userEmail = $('#user_email');
   var $sections = $('.form-section');
+  var $checkBoxes = $('.subject-settings-section input');
   $submitButton.attr('disabled', '');
+
+  // Displays greyed out checkboxes
+  $checkBoxes.each(function() {
+    $this = $(this);
+    $this.attr('disabled', '');
+    $this.prop('checked', true);
+  });
 
   // Prepare sections by setting the `data-parsley-group` attribute to 'block-0', 'block-1', etc.
   $sections.each(function(index, section) {
@@ -17,6 +25,15 @@ $(document).on('turbolinks:load', function() {
 
   $loginButton.click(function() {
     tryToLogin();
+  });
+
+  // Repurpose Enter key for submitting email
+  $userEmail.on('keyup keypress', function(e) {
+    var key = e.keyCode || e.which;
+    if (key == 13) {
+      e.preventDefault();
+      tryToLogin();
+    }
   });
 
   // Submits email and shows checkboxes iff current block validates
@@ -30,35 +47,33 @@ $(document).on('turbolinks:load', function() {
 
       // Check email with AJAX
       var email = $userEmail.val();
-      showCheckboxes(email);
+      enableCheckboxes(email);
     }
   }
 
-  function showCheckboxes(email) {
+  function enableCheckboxes(email) {
     // POST to get user settings, or default settings if new user
     $.post('/login', {'email': email}, function(data, status) {
       if (status == "success" && Array.isArray(data)) {
+
+        // Enables checkboxes and unchecks all of them
+        $checkBoxes.each(function() {
+          $this = $(this);
+          $this.removeAttr('disabled');
+          $this.prop('checked', false);
+        });
         
-        // Loads checkboxes
+        // Checks checkboxes based on user settings
         for (var i = 0; i < data.length; i++) {
           $('input[value="' + data[i] + '"]').prop('checked', true);
         }
 
         // Shows checkboxes
-        $('.subject-settings-section').show();
+        // $('.subject-settings-section').show();
 
         // Enable submit button
         $submitButton.removeAttr('disabled');
       }
     }, 'json');
   }
-
-  // Repurpose Enter key for inputting email
-  $userEmail.on('keyup keypress', function (e) {
-    var key = e.keyCode || e.which;
-    if (key == 13) {
-      e.preventDefault();
-      tryToLogin();
-    }
-  });
 });
